139244dcb4cfc3ab5bd46a8a93b7d6f895a596ef
ScottHull <scottdhull21@gmail.com>
2021-12-14 13:45:00 -0500

    initial

--- eos.py
+++ eos.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.style.use("dark_background")
+
 def birch_murnaghan_3rd_order(V, V0=6.73, K0=155.8, K0_prime=5.81):
     """
     Third-order birtch-Murnaghan isothermal EoS, P(V).
@@ -35,19 +37,59 @@ def pressure(V, deltaT):
     """
     return birch_murnaghan_3rd_order(V) + P_th(deltaT)
 
-def V2(V1, assumed_alpha, T1, T2):
+def expanded_volume(V1, assumed_alpha, T1, T2):
     deltaV = assumed_alpha * V1 * (T2 - T1)  # integrated thermal expansion equation for volume
     return V1 + deltaV  # by definition, V1 = V1 + deltaV
 
-print(pressure())
 
-# df = pd.read_csv("anderson_iron_300k.xlsx")
-# temps = [300] + list(np.arange(1000, 7000 + 1000, 1000))
-# for row in df.index:
-#     for index, T1 in enumerate(temps):
-#         if index + 1 < len(temps):
-#             isobar_pressure = df["P"][row]
-#             V_1 = df["V"][row]
-#             T2 = temps[index + 1]
-#
+def get_v_from_isotherm(T, target_P):
+    """
+    Returns the volume of a target P from an isotherm.
+    :param T:
+    :param target_P:
+    :return:
+    """
+    volumes = list(np.arange(3.9, 7 + 0.1, 0.1))
+    isobar = [pressure(V=V, deltaT=(T - 300)) for V in volumes]
+    min_val = 1e10
+    target_V = None
+    for index, i in enumerate(isobar):
+        test = abs(i - target_P)
+        if test < min_val:
+            min_val = test
+            target_V = volumes[index]
+    return target_V
 
+alphas = np.arange(1e-6, 5e-5, 1e-6)
+temperatures = np.arange(1000, 8000, 1000)
+pres = []
+v2s = []
+target_P = 181.7
+v1 = 0.68
+T1 = 300
+found_alphas = []
+for T2 in temperatures:
+    target_v = get_v_from_isotherm(T=T2, target_P=target_P)
+    candidate_alpha = 1e99
+    candidate_v2 = 1e99
+    min_diff = 1e99
+    for alpha in alphas:
+        v2 = expanded_volume(V1=v1, assumed_alpha=alpha, T1=T1, T2=T2)
+        test = abs(v2 - target_v)
+        if test < min_diff:
+            candidate_alpha = alpha
+            candidate_v2 = v2
+            min_diff = test
+    found_alphas.append(candidate_alpha * 10 ** 5)
+    v2s.append(candidate_v2)
+
+
+fig = plt.figure(figsize=(16, 9))
+ax = fig.add_subplot(111)
+ax.plot(
+    temperatures,
+    found_alphas,
+    linewidth=2.0
+)
+
+plt.show()
